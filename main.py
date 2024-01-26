6952f49f713fb53a1b3fc3c7e4c3e78952f6766a
Tyujhty <fannybaquey@gmail.com>
2024-01-26 16:59:38 +0100

    feat: Add constant for the word to replace.

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+WORD_TO_REPLACE = "[name]"
+
 names = open("./Input/Names/invited_names.txt")
 letter = open("./Input/Letters/starting_letter.txt", "r").read()
+
 
 for name in names:
     new_name = name.strip()
@@ -7,7 +10,7 @@ for name in names:
     new_letter_name = f"invitation_{name}.txt"
 
     new_file_path = output_path + new_letter_name
-    new_letter = letter.replace("[name]", new_name)
+    new_letter = letter.replace(WORD_TO_REPLACE, new_name)
 
     with open(new_file_path, "x") as modified_letter:
         modified_letter.write(new_letter)
