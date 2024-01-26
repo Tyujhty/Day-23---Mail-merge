07260cd88c8f408edbb91cf93e5a4c591df573f2
Tyujhty <fannybaquey@gmail.com>
2024-01-26 16:52:06 +0100

    feat: Use Open() to read names and letter. Loop through the names. Create new name for letter and indicate where to save them.

--- main.py
+++ main.py
@@ -6,3 +6,16 @@
 #Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
     #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
         #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
+names = open("./Input/Names/invited_names.txt")
+letter = open("./Input/Letters/starting_letter.txt", "r").read()
+
+for name in names:
+    new_name = name
+    output_path = "./Output/ReadyToSend/"
+    new_letter_name = f"invitation_{name}.txt"
+
+    new_file_path = output_path + new_letter_name
+    new_letter = letter.replace("[name]", new_name)
+
+    with open(new_file_path, "x") as modified_letter:
+        modified_letter.write(new_letter)
