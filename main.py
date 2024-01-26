8acbd6072c64c37ac16b02a9cd99d235c7db8fb2
Tyujhty <fannybaquey@gmail.com>
2024-01-26 16:57:14 +0100

    feat: Use strip() to remove \n in names.

--- main.py
+++ main.py
@@ -1,16 +1,8 @@
-#TODO: Create a letter using starting_letter.txt 
-#for each name in invited_names.txt
-#Replace the [name] placeholder with the actual name.
-#Save the letters in the folder "ReadyToSend".
-    
-#Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
-    #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
-        #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
 names = open("./Input/Names/invited_names.txt")
 letter = open("./Input/Letters/starting_letter.txt", "r").read()
 
 for name in names:
-    new_name = name
+    new_name = name.strip()
     output_path = "./Output/ReadyToSend/"
     new_letter_name = f"invitation_{name}.txt"
 
